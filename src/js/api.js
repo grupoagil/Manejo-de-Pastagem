e9e17c6dd54d01ce78364d51b41fe7aa1aa6f89f
danielpinon <danielpinon@danielpinon.com.br>
2022-08-23 11:36:59 -0300

    Feat: Atualização de nomes de pagina e de funcao de visibilidade

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,5 @@
 import Framework7 from 'framework7/bundle';
+import $$ from "dom7";
 let $f7 = Framework7;
 
 var api = {
@@ -30,7 +31,48 @@ var api = {
             });
             return promessa;
         }
-    }
+    },
+    pageIsVisible: (id, fn) => {
+        var visivel = false;
+        setInterval(() => {
+            if (isVisible($$(id)[0]) == true) {
+                if (visivel == false) {
+                    fn();
+                    visivel = true;
+                }
+            } else {
+                visivel = false;
+            }
+        }, 500);
+    },
 };
+
+// Verifica se a tela está visivel
+function isVisible(elem) {
+    if (!(elem instanceof Element)) throw Error('DomUtil: elem is not an element.');
+    const style = getComputedStyle(elem);
+    if (style.display === 'none') return false;
+    if (style.visibility !== 'visible') return false;
+    if (style.opacity < 0.1) return false;
+    if (elem.offsetWidth + elem.offsetHeight + elem.getBoundingClientRect().height +
+        elem.getBoundingClientRect().width === 0) {
+        return false;
+    }
+    const elemCenter   = {
+        x: elem.getBoundingClientRect().left + elem.offsetWidth / 2,
+        y: elem.getBoundingClientRect().top + elem.offsetHeight / 2
+    };
+    if (elemCenter.x < 0) return false;
+    if (elemCenter.x > (document.documentElement.clientWidth || window.innerWidth)) return false;
+    if (elemCenter.y < 0) return false;
+    if (elemCenter.y > (document.documentElement.clientHeight || window.innerHeight)) return false;
+    let pointContainer = document.elementFromPoint(elemCenter.x, elemCenter.y);
+    do {
+        if (pointContainer === elem) return true;
+    } while (pointContainer = pointContainer.parentNode);
+    return false;
+}
+
+
 
 export default api;
