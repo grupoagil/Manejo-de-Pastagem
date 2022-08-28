6f9f9a11eed462ea1557a4e4a2439dd3281ff149
danielpinon <danielpinon@danielpinon.com.br>
2022-08-28 09:46:08 -0300

    Feat: Ajustes da api de indentificar se está visível a página

--- src/js/api.js
+++ src/js/api.js
@@ -33,11 +33,21 @@ var api = {
         }
     },
     pageIsVisible: (id, fn) => {
+        if (window.pageIsVisible == null) {
+            window.pageIsVisible = false;
+        }
         var visivel = false;
         setInterval(() => {
             if (isVisible($$(id)[0]) == true) {
                 if (visivel == false) {
-                    fn();
+                    if (!window.pageIsVisible) {
+                        window.pageIsVisible=true;
+                        setTimeout(()=>{
+                          window.pageIsVisible=false;
+                        },1000);
+                        // Atualiza página quando estiver visível
+                        fn();
+                    }
                     visivel = true;
                 }
             } else {
@@ -49,28 +59,32 @@ var api = {
 
 // Verifica se a tela está visivel
 function isVisible(elem) {
-    if (!(elem instanceof Element)) throw Error('DomUtil: elem is not an element.');
-    const style = getComputedStyle(elem);
-    if (style.display === 'none') return false;
-    if (style.visibility !== 'visible') return false;
-    if (style.opacity < 0.1) return false;
-    if (elem.offsetWidth + elem.offsetHeight + elem.getBoundingClientRect().height +
-        elem.getBoundingClientRect().width === 0) {
+    try {
+        if (!(elem instanceof Element)) throw Error('DomUtil: elem is not an element.');
+        const style = getComputedStyle(elem);
+        if (style.display === 'none') return false;
+        if (style.visibility !== 'visible') return false;
+        if (style.opacity < 0.1) return false;
+        if (elem.offsetWidth + elem.offsetHeight + elem.getBoundingClientRect().height +
+            elem.getBoundingClientRect().width === 0) {
+            return false;
+        }
+        const elemCenter   = {
+            x: elem.getBoundingClientRect().left + elem.offsetWidth / 2,
+            y: elem.getBoundingClientRect().top + elem.offsetHeight / 2
+        };
+        if (elemCenter.x < 0) return false;
+        if (elemCenter.x > (document.documentElement.clientWidth || window.innerWidth)) return false;
+        if (elemCenter.y < 0) return false;
+        if (elemCenter.y > (document.documentElement.clientHeight || window.innerHeight)) return false;
+        let pointContainer = document.elementFromPoint(elemCenter.x, elemCenter.y);
+        do {
+            if (pointContainer === elem) return true;
+        } while (pointContainer = pointContainer.parentNode);
+        return false;
+    } catch (error) {
         return false;
     }
-    const elemCenter   = {
-        x: elem.getBoundingClientRect().left + elem.offsetWidth / 2,
-        y: elem.getBoundingClientRect().top + elem.offsetHeight / 2
-    };
-    if (elemCenter.x < 0) return false;
-    if (elemCenter.x > (document.documentElement.clientWidth || window.innerWidth)) return false;
-    if (elemCenter.y < 0) return false;
-    if (elemCenter.y > (document.documentElement.clientHeight || window.innerHeight)) return false;
-    let pointContainer = document.elementFromPoint(elemCenter.x, elemCenter.y);
-    do {
-        if (pointContainer === elem) return true;
-    } while (pointContainer = pointContainer.parentNode);
-    return false;
 }
 
 
