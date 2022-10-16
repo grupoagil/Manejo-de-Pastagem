25e960cdf3dd56b0511cf2e6f32061d1890bdf65
danielpinon <danielpinon@danielpinon.com.br>
2022-10-15 21:58:36 -0300

    Feat: Adicionando informações de alerta no aplicativo

--- src/js/api.js
+++ src/js/api.js
@@ -3,7 +3,7 @@ import $$ from "dom7";
 let $f7 = Framework7;
 
 var api = {
-    base:"https://144.91.127.223:49156/api",
+    base:"http://localhost:8000/api",
     auth:{
         login:($json={})=>{
            // Login User 
